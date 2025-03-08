f731b8b7ce353dcce27a4c1fcf26edbf886cc645
CamilaFre <camila.frech@hotmail.com>
2025-03-08 20:34:12 -0300

    exercicio 06

---
+++ ex_06.py
@@ -0,0 +1,6 @@
+#Crie um programa que peça a largura e o comprimento de um terreno e calcule sua área.
+
+largura = float(input("Digite a largura do terreno: "))
+comprimento = float (input("Digite o comprimento do terreno: "))
+area = largura*comprimento
+print(f"A area do terreno é {area}.")
