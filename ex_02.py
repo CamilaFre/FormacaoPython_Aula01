a803899cf567a29ccc4312a57badba795293f58f
CamilaFre <camila.frech@hotmail.com>
2025-03-08 20:08:48 -0300

    exercico 02

---
+++ ex_02.py
@@ -0,0 +1,6 @@
+#Crie um programa que peça um número inteiro e um número float e exiba a soma dos dois.
+
+n1 = int(input("Digite um número inteiro"))
+n2 = float(input("Digite um número com casa decimal"))
+soma = n1+n2
+print(f"A soma do {n1} e do {n2} é {soma}. ")
