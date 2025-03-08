61009a3ffeef3ef8ea792dcfc8062f43f65bbd15
CamilaFre <camila.frech@hotmail.com>
2025-03-08 19:46:01 -0300

    exercico 01

---
+++ ex_01.py
@@ -0,0 +1,4 @@
+#Crie um programa que pergunte o nome e a idade do usuário e mostre a seguinte mensagem: "Seu nome é {nome} e você tem {idade} anos"
+nome = input("Qual é o seu nome?")
+idade = int(input("Qual é a sua idade?"))
+print(f"Seu nome é {nome} e você tem {idade}")
