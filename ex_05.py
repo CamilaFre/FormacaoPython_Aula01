b0ebc7abb4f9547cff195629f3ff48b5c8bcd74e
CamilaFre <camila.frech@hotmail.com>
2025-03-08 20:29:25 -0300

    exercicio 05

---
+++ ex_05.py
@@ -0,0 +1,10 @@
+#Crie um programa que solicite um número inteiro e mostre o dobro, o triplo e a raiz quadrada desse número.
+
+numero = int(input("Digite um número: "))
+dobro = numero*2
+triplo = numero*3
+raiz  = int(numero**0.5)
+
+print(f"O dobro de {numero} é {dobro}, o triplo é {triplo}, e a raiz quadrada é {raiz}.")
+
+
