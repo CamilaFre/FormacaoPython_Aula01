5de05a7f8d1ac3f6d8a70444a2cdf164fa9e2a6b
CamilaFre <camila.frech@hotmail.com>
2025-03-08 20:19:31 -0300

    exercicio 04

---
+++ ex_04.py
@@ -0,0 +1,5 @@
+#Crie um programa que peça uma frase e informe quantos caracteres ela possui.
+
+frase = input("Digite uma frase: ")
+letras = len(frase)
+print(f"A frase {frase} tem {letras} caracteres.")
