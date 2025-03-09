4823965d2b5648b3d415414f0eb8b85279e94ae7
CamilaFre <camila.frech@hotmail.com>
2025-03-08 21:08:38 -0300

    exercico 07

---
+++ ex_07.py
@@ -0,0 +1,4 @@
+#Crie um programa que solicite um valor em metros e converta esse valor para centímetros.
+valor = float(input("Digite um valor em cm: "))
+metros = float(valor/100)
+print(f"O valor de {valor} em metros é {metros}.")
