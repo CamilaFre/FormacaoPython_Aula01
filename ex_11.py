f277070094a23b7c9cab2d5a2c22b97a87c6fb56
CamilaFre <camila.frech@hotmail.com>
2025-03-09 19:26:28 -0300

    exercicio 11

---
+++ ex_11.py
@@ -0,0 +1,8 @@
+#Crie um programa que solicite um número float e exiba o valor elevado ao quadrado e ao cubo.
+
+n1= float(input("Digite um número decimal: "))
+
+quadrado = n1**2
+cubo = n1**3
+
+print(f"O quadrado de {n1} é {quadrado} e o cubo {cubo}.")
