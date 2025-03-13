b87e1ce6350ae904a2628cb0dc1165cb0ec5376b
CamilaFre <camila.frech@hotmail.com>
2025-03-13 20:05:55 -0300

    exercicio 12

---
+++ ex_12.py
@@ -0,0 +1,8 @@
+## Crie um programa que solicite o salário de um funcionário e calcule um aumento de 15% sobre o salário.
+
+salario = float(input("Digite seu salário: "))
+
+aumento = salario*1.15
+
+print(f"Seu salário foi de {salario} para {aumento}.")
+
