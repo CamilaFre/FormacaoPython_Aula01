98fa07a962c3c6c41e9f8c28da427357d37abc75
CamilaFre <camila.frech@hotmail.com>
2025-03-14 20:55:38 -0300

    Exercicio 17

---
+++ ex_17.py
@@ -0,0 +1,8 @@
+##Crie um programa que solicite duas palavras (nome e sobrenome) e as exiba juntas, separadas por um espaço.
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+completo = nome + " " + sobrenome
+
+print(completo)
