28a56062e5539b6adcfb5302ee319643ba105f8d
CamilaFre <camila.frech@hotmail.com>
2025-03-14 21:09:29 -0300

    exercico 18

---
+++ ex_18.py
@@ -0,0 +1,8 @@
+# Crie um programa que solicite um número inteiro e exiba seu antecessor e sucessor.
+
+n = int(input("Digite um número: "))
+
+ant = n-1
+suc = n+1
+
+print(f"O antecessor de {n} é {ant} e o sucessor é {suc}.")
